2d188f4df74b2bdaa622947126a873c64dfee0da
kmj <kmjk1117@gmail.com>
2025-03-22 15:25:09 +0900

    section11

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,8 +1,46 @@
+import Editor from './components/Editor'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  return <div className="App">Hello, world!</div>
+interface Todo {
+  id: number;
+  content: string;
+}
 
+function App() {
+
+  // todo를 보관할 스테이트
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // id값 초기화
+  const idRef = React.useRef(0);
+
+  // onChangeAdd 함수
+  // setText의 상태를 변경해야하므로 App.tsx에 선언
+  const onClickAdd = (text:string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+    // // 입력창 초기화
+    // setText('');
+  }
+
+  // todos가 변경될 때마다 실행
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd}/>
+      <div>child</div>
+    </div>
+  );
 }
 
 export default App
---
+++ section11/src/components/Editor.tsx
@@ -0,0 +1,33 @@
+import React, {useState } from 'react';
+
+// props의 타입 정의
+interface EditorProps {
+  onClickAdd: (text: string) => void;
+}
+
+export default function Editor(props: EditorProps) {
+    // useState = 제네릭 함수
+    // useState의 인자로는 상태의 초기값을 전달..()에 아무 값도 없으면 undefined으로 추론됨
+    // 즉, useState() 인자의 초기값으로 '' 와 같이 인자를 할당을 권장
+    const [text, setText] = useState('');
+
+    // e: React.ChangeEvent<HTMLInputElement> = 이벤트 객체의 타입을 명시
+    const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setText(e.target.value);
+    };
+
+    const onClickButton = () => {
+      props.onClickAdd(text);
+      // 입력창 초기화
+      setText('');  
+    };
+
+  return (
+    <div>
+      {/* input 태그의 value 속성에 text 상태를 할당하면 text에 저장됨 */}
+      <input value={text} onChange={onChangeInput} />
+      <button onClick={onClickButton}>추가</button>
+    </div>
+    );
+  }
+  
